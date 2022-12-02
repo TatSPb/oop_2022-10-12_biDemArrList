10ae757fd13226f1fdd2acb29ece381b4cdd0dd1
TatSPb <t-sverchkova@yandex.ru>
2022-12-03 00:38:07 +0300

    Tasl completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        example();
+        example2();
     }
+        public  static  void example() {
+            List<List<String>> biDemArrList = new ArrayList<>();
+            System.out.println("Задание-1:");
+            //заполнение:
+            for (int i = 0; i < 8; i++) {
+                biDemArrList.add(i, new ArrayList<>());
+                for (int j = 0; j < 8; j++) {
+                    biDemArrList.get(i).add(j, "◯");
+                }
+            }
+            //вывод:
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    System.out.print(biDemArrList.get(i).get(j) + " ");
+                }
+                System.out.println(); // перенос после каждой строки
+            }
+        }
+
+    public  static  void example2() {
+        List<List<String>> biDemArrList = new ArrayList<>();
+        System.out.println("Задание-2:");
+        for (int i = 0; i < 8; i++) {
+            biDemArrList.add(i, new ArrayList<>());
+            for (int j = 0; j < 8; j++) {
+                biDemArrList.get(i).add(j, (i + j) % 2 == 1 ? "*" : "◯");
+            }
+        }
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print(biDemArrList.get(i).get(j) + " ");
+            }
+            System.out.println(); // перенос после каждой строки
+        }
+    }
+
 }
